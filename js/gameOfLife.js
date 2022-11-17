f509f41c7f9012bb902b326e1a40d7906e043826
estefaniacr <estefania.c.r@hotmail.com>
2022-11-17 15:05:56 +0100

    Code and test finished, modification .editconfig

--- js/gameOfLife.js
+++ js/gameOfLife.js
@@ -7,28 +7,92 @@
 // Regla célula muerta:
 //    - Permanece muerta hasta que tiene exactamente células contiguas vivas, momento en el que nace.
 
+//Calculamos un numero random entero entre el valor asignado al max
+export function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 //Create grid size
-export function createGrid() {
-    const grid = [];
-    const columns = 9;
-    const rows = 9;
-    for (let i = 0; i < rows; i++) {
-        grid.push([]);
-        for (let j = 0; j < columns; j++) {
-            grid[i].push(0);
+export function createGrid(columns, rows) {
+    //empty grid
+    const array = new Array(columns); // eje x
+    for (let i = 0; i < array.length; i++) {
+        array[i] = new Array(rows); // eje y
+    }
+    return array; //devolvemos matriz y evitamos 0 -> undefined
+}
+
+//Valores por defecto dimension grid
+let grid;
+const columns = 9;
+const rows = 9;
+
+//Funcion principal Conway's game of life
+export function GoT() {
+    grid = createGrid(columns, rows); //dimension grid
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            //recorremos y rellenamos matriz con valores random de 0-1
+            grid[i][j] = getRandomInt(2);
         }
     }
     return grid;
 }
 
-//Create second grid
-//Create method checkAliveNextCell
+//Count live neighbors (value 1)
+export function countNeighbors(array, i, j) {
+    let contLiveCells = 0;
+
+    if (i !== 0) {
+        contLiveCells += array[i - 1][j + 1];
+        contLiveCells += array[i - 1][j - 1];
+        contLiveCells += array[i - 1][j];
+    }
+    if (i !== 1) {
+        contLiveCells += array[i][j + 1];
+        contLiveCells += array[i][j - 1];
+    }
+    if (i + 1 !== array.length) {
+        contLiveCells += array[i + 1][j + 1];
+        contLiveCells += array[i + 1][j - 1];
+        contLiveCells += array[i + 1][j];
+    }
+    return contLiveCells;
+}
+
+export function mirrorArray(array, columns, rows) {
+    const secondArray = [].concat(createGrid(columns, rows)); //concatena createGrid dentro del array vacio secondArray
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            //celulas muertas
+            if (array[i][j] === 0) {
+                //Num secondArray = cont de celulas vecinas es 3 (true 1/false 0)
+                secondArray[i][j] = countNeighbors(array, i, j) === 3 ? 1 : 0;
+            } else {
+                //celulas vivas
+                //Num secondArray = cont de celulas vecinas esta entre 1-4 (true 1/false 0)
+                secondArray[i][j] =
+                    countNeighbors(array, i, j) > 1 &&
+                    countNeighbors(array, i, j) < 4
+                        ? 1
+                        : 0;
+            }
+        }
+    }
+    return secondArray;
+}
+
+const gameOfLife = () => {
+    const outputGrid = GoT();
+    console.table(outputGrid);
+};
+gameOfLife();
 
 //Comentado por async (test con jest)
-// setInterval(() => {
-//     //!Modificación de array, ahora esta imprimiendo solo la matriz "madre"  -->13/11
-//     //Print table
-//     const grid = createGrid();
-//     console.table(grid);
-//     console.log('-----------------------------------------------');
-// }, 1000);
+// const gameOfLife = () => {
+//     setInterval(() => {
+//         const outputGrid = GoT();
+//         console.table(outputGrid);
+//     }, 1000);
+// };
+// gameOfLife();
--- js/gameOfLife.test.js
+++ js/gameOfLife.test.js
@@ -1,10 +1,118 @@
-import { createGrid } from './gameOfLife';
+import {
+    getRandomInt,
+    createGrid,
+    GoT,
+    countNeighbors,
+    mirrorArray,
+} from './gameOfLife';
+
+describe('Given getRandomInt function', () => {
+    test('Get a integer random number between 0 and 1', () => {
+        const result = getRandomInt(13);
+        const expected = result >= 1 || result <= 13;
+        expect(expected).toBe(true);
+    });
+});
 
 describe('Given createGrid function', () => {
-    test('Return a grid with all 0', () => {
+    test('Return a grid with all the boxes with value 0', () => {
         const grid = createGrid(3, 3);
-        const expected = 0;
-        const result = grid[0][8];
+        const expected = undefined;
+        const result = grid[0][7];
         expect(result).toEqual(expected);
+    });
+    test('Return a grid with length 7', () => {
+        const result = 7;
+        const expected = createGrid(7, 7).length;
+        expect(expected).toEqual(result);
+    });
+});
+
+describe('Given GoT function', () => {
+    const grid = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    ];
+    test('Return a grid with the same length (length 9x9), result should be true', () => {
+        const result = grid.length;
+        const expected = GoT().length === result;
+        expect(expected).toBe(true);
+    });
+    test.each(grid)(
+        'Return a grid includes the number 1, result should be true',
+        () => {
+            const result = grid.includes(1);
+            const expected = GoT().includes(1);
+            expect(expected).toBe(result);
+        }
+    );
+});
+
+describe('Given countNeighbor function', () => {
+    test('When it receives its coordinates it checks for dead neighbors', () => {
+        const grid = [
+            [0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+        const row = 7;
+        const colum = 5;
+        const result = countNeighbors(grid, row, colum);
+        expect(result).toBe(0);
+    });
+    test('When it receives its coordinates it checks the living neighbors', () => {
+        const grid = [
+            [0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+        const row = 5;
+        const colum = 5;
+        const result = countNeighbors(grid, row, colum);
+        expect(result).toBe(3);
+    });
+});
+
+describe('Given mirrorArray function', () => {
+    const grid = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ];
+    const row = 7;
+    const colum = 7;
+    test('The result given the grid, the numbers 1 would change from vertical to horizontal view', () => {
+        const result = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ];
+        const expected = mirrorArray(grid, row, colum);
+        expect(expected).toEqual(result);
     });
 });
